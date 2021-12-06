fa280baea33cb810d2b328a1cdf5138618c8c003
Sarah Akbar <sarahakbar@Sarahs-MacBook-Pro.local>
2021-12-06 13:53:23 -0500

    added gitignore

---
+++ elena-frontend/src/view/MapView.js
@@ -0,0 +1,47 @@
+
+import React, { useState, useRef, useEffect } from 'react';
+
+import config from '../assets/mapbox.json'
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
+// eslint-disable-next-line import/no-webpack-loader-syntax
+mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
+
+const MAPBOX_TOKEN = config.REACT_APP_TOKEN;
+
+const MapView = () => {
+
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
+
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/dark-v10',
+            center: [lng, lat],
+            zoom: zoom,
+            accessToken: MAPBOX_TOKEN
+        });
+    });
+
+
+    return (
+        <div>
+            <div ref={mapContainer} className="map-container" />
+        </div>
+    );
+
+
+}
+
+
+
+export default MapView
