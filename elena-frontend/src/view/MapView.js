72d97029b4d4ab6dfbd109ffe4eb1dee73f361a9
Sarah Akbar <sarahakbar@Sarahs-MacBook-Pro.local>
2021-12-06 20:37:50 -0500

    Takes user input and sends to the controller/backend

--- elena-frontend/src/view/MapView.js
+++ elena-frontend/src/view/MapView.js
@@ -3,7 +3,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import "./MapViewStyles.scss"
 
 import config from '../assets/mapbox.json'
-import mapboxgl from 'mapbox-gl';
+import mapboxgl, { accessToken } from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
@@ -11,42 +11,36 @@ import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 // eslint-disable-next-line import/no-webpack-loader-syntax
 mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
-
-mapboxgl.accessToken = 'pk.eyJ1Ijoic2FrYmFyIiwiYSI6ImNrd3BpZ3R5dDBkNmwydnM2MGczZWczejMifQ.ogaGLHvGYHqJ8Y8ThXf8yQ';
+const tkn = 'pk.eyJ1Ijoic2FrYmFyIiwiYSI6ImNrd3BpZ3R5dDBkNmwydnM2MGczZWczejMifQ.ogaGLHvGYHqJ8Y8ThXf8yQ';
+mapboxgl.accessToken = tkn
 
 const MapView = () => {
 
     const mapContainer = useRef(null);
-    const map = useRef(null);
     const [lng, setLng] = useState(-72.526711);
     const [lat, setLat] = useState(42.391155);
     const [zoom, setZoom] = useState(14);
 
     useEffect(() => {
-        if (map.current) return; // initialize map only once
-        map.current = new mapboxgl.Map({
+        const map = new mapboxgl.Map({
             container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v11',
+            style: 'mapbox://styles/mapbox/dark-v10',
             center: [lng, lat],
-            zoom: zoom
+            zoom: zoom,
         });
-    });
 
-    useEffect(() => {
-        if (!map.current) return; // wait for map to initialize
-        map.current.on('move', () => {
-            setLng(map.current.getCenter().lng.toFixed(4));
-            setLat(map.current.getCenter().lat.toFixed(4));
-            setZoom(map.current.getZoom().toFixed(2));
+
+        map.on('move', () => {
+            setLng(map.getCenter().lng.toFixed(4));
+            setLat(map.getCenter().lat.toFixed(4));
+            setZoom(map.getZoom().toFixed(2));
         });
+
     });
 
 
     return (
         <div className="mapView">
-            <div className="sidebar">
-                Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-            </div>
             <div ref={mapContainer} className="map-container" />
         </div>
     );
