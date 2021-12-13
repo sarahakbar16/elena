7d397f7252900a21321cd0a1e5330b927b285ae4
Janhvi Joshi <500060253@stu.upes.ac.in>
2021-12-12 22:53:29 -0500

    Added code to receive data from UI.

--- models/get_path.py
+++ models/get_path.py
@@ -1,4 +1,7 @@
 # from typing_extensions import Required
+from map_utility import Map
+from algorithms_utility import Algorthms
+
 from flask import request
 from flask_restful import Resource, reqparse
 
@@ -10,57 +13,65 @@ import networkx as nx
 class GetPath(Resource):
 
     parser = reqparse.RequestParser()
-    parser.add_argument('source',
+    parser.add_argument("source_coords_lat",
                         type=str,
                         required=True,
                         help="This field cannot be blank."
                         )
-    parser.add_argument('destination',
+    parser.add_argument("source_coords_long",
                         type=str,
                         required=True,
                         help="This field cannot be blank."
                         )
-    parser.add_argument('is_elevation_max',
+    parser.add_argument("destination_coords_lat",
                         type=str,
                         required=True,
                         help="This field cannot be blank."
                         )
-    parser.add_argument('percentage',
+    parser.add_argument("destinations_coords_long",
                         type=str,
                         required=True,
                         help="This field cannot be blank."
                         )
-    parser.add_argument('algorithm',
+    parser.add_argument("is_elevation_max",
                         type=str,
                         required=True,
                         help="This field cannot be blank."
                         )
+    parser.add_argument("percentage",
+                        type=str,
+                        required=True,
+                        help="This field cannot be blank."
+                        )
+    parser.add_argument("algorithm",
+                        type=str,
+                        required=True,
+                        help="This field cannot be blank."
+                        )
+
 
 
     def post(self):
         data = self.parser.parse_args()
         
-        source = "umass police station"
-        destintion = "puffton village apartments"
+        source_coords_lat = float(data["source_coords_lat"])
+        source_coords_long = float(data["source_coords_long"])
+        source_coords = (source_coords_lat, source_coords_long)
 
-        source_coord = osmnx.geocoder.geocode(source)
-        destination_coord = osmnx.geocoder.geocode(destintion)
-        is_max = True
-        percentage = 50
-        print(source_coord)
-        print(destination_coord)
+        destination_coords_lat = float(data["destination_coords_lat"])
+        destinations_coords_long = float(data["destinations_coords_long"])
+        destination_coords = (destination_coords_lat, destinations_coords_long)
 
-        params = dict()
-        params['city']  = "Amherst"
-        params['state'] = "Massachusetts"
-        params['country'] = 'USA'
-        graph = osmnx.graph_from_place(params, network_type='drive')
+        is_max = data["is_elevation_max"]
+        percentage = int(data["percentage"])
+        algorithm = data["algorithm"]
 
-        # call methods to 
-        # gen map
-        # find node ids
-        # add elevation
-        # call algo
+        if algorithm == "True":
+            algorithm = True
+        else:
+            algorithm = False
+
+        
 
         return None
 
